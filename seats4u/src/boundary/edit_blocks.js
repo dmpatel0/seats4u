d1d188261b988606207bdd42419a11c90b69fabd
H4C1M <micahvargas@icloud.com>
2023-12-13 14:32:09 -0500

    More block functionality

--- seats4u/src/boundary/edit_blocks.js
+++ seats4u/src/boundary/edit_blocks.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import { listBlocks } from '../controller/controllerVenueManager'
 import { currentVenue } from './venue-view'
-import { currentShow } from './venue-view'
 import { useNavigate } from 'react-router-dom';
 
 export let wipBlocks; // use json
@@ -57,22 +56,28 @@ function createBlockHandler(){
 }
 
 function addBlockHandler(){
-    let blockID = 10;
-    let showID = currentShow;
+    let blockID = 10; // make new id
+    let showID = 2; // get showID
     let sectionID = document.getElementById("inp-sectionID").value;
     let bStartRow = document.getElementById("inp-startRow").value;
     let bEndRow = document.getElementById("inp-endRow").value;
     let bPrice = document.getElementById("inp-price").value;
 
-    // Add to wipBlocks
+    wipBlocks.push({"blockID":blockID, "showID":showID, "sectionID":sectionID, "blockStartRow":bStartRow, "blockEndRow":bEndRow, "blockPrice":bPrice});
 
     refreshHandler();
 }
 
 function deleteBlockHandler(){
-    let blockID = document.getElementById("inp-blockID").value;
+    let bID = document.getElementById("inp-blockID").value;
 
     // remove from wipBlocks
+    for(let i = 0; i < wipBlocks.length; i++){
+        if(wipBlocks[i].blockID == bID){
+            delete wipBlocks[i];
+            break;
+        }
+    }
 
     refreshHandler();
 }
@@ -83,55 +88,56 @@ const EditBlocks = () => {
 
     useEffect(() => {
         document.getElementById("venue-name-label").innerHTML = currentVenue;
+        document.getElementById("show-name-label").innerHTML = 10;
     })
 
     return (
-        <div id="view-container" className="venue-view">
-        <button id="btn-back-create-venue" class="btn-back" onClick={() => {navigate('/venue-view')}}>BACK</button>
-        <h1 id="venue-view-name">WPI</h1>
-        <label id="label-show-id">TEST</label>
-        <div id="blocks-list">
-            <div id="blocks-view-list-div">
-                <h2>CURRENT BLOCKS</h2>
+        <div class="edit-blocks">
+            <button id="btn-back-create-venue" class="btn-back" onClick={() => {navigate('/venue-view')}}>BACK</button>
+            <h1 id="venue-name-label"></h1>
+            <label id="show-name-label"></label>
+            <div id="blocks-list">
                 <div id="blocks-view-list-div">
-                    <div></div>
+                    <h2>CURRENT BLOCKS</h2>
+                    <div id="blocks-view-list-div">
+                        <div></div>
+                    </div>
                 </div>
+                <button id="btn-create-block" onClick={() => {createBlockHandler()}}>CREATE</button>
+                <p id="api-result"></p>
+                <button id="btn-reset-blocks" onClick={() => {listBlocksHandler()}}>UNDO CHANGES</button>
             </div>
-            <button id="btn-create-block" onClick={() => {createBlockHandler()}}>CREATE</button>
-            <button id="btn-reset-blocks" onClick={() => {listBlocksHandler()}}>UNDO CHANGES</button>
+            <div id="add-block">
+                <div id="venueName">
+                    <h2>VENUE NAME</h2>
+                    <label id="venue-name-label"></label>
+                </div>
+                <div id="sectionID">
+                    <h2>SECTION</h2>
+                    <input id="inp-sectionID" placeholder="Section"></input>
+                </div>
+                <div id="startRow">
+                    <h2>START ROW</h2>
+                    <input id="inp-startRow" placeholder="Starting Row"></input>
+                </div>
+                <div id="endRow">
+                    <h2>END ROW</h2>
+                    <input id="inp-endRow" placeholder="Ending Row"></input>
+                </div>
+                <div id="blockPrice">
+                    <h2>PRICE</h2>
+                    <input id="inp-price" placeholder="Price within block"></input>
+                </div>
+                <button id="btn-add-block" onClick={() => {addBlockHandler()}}>ADD</button>
+            </div>
+            <div id="delete-block">
+                <div id="blockID">
+                    <h2>BLOCK ID</h2>
+                    <input id="inp-blockID" placeholder="Block ID Number"></input>
+                </div>
+                <button id="btn-delete-block" onClick={() => {deleteBlockHandler()}}>DELETE</button>
+            </div>        
         </div>
-        <div id="add-block">
-            <div id="venueName">
-                <h2>VENUE NAME</h2>
-                <label id="venue-name-label"></label>
-            </div>
-            <div id="sectionID">
-                <h2>SECTION</h2>
-                <input id="inp-sectionID" placeholder="Section"></input>
-            </div>
-            <div id="startRow">
-                <h2>START ROW</h2>
-                <input id="inp-startRow" placeholder="Starting Row"></input>
-            </div>
-            <div id="endRow">
-                <h2>END ROW</h2>
-                <input id="inp-endRow" placeholder="Ending Row"></input>
-            </div>
-            <div id="blockPrice">
-                <h2>PRICE</h2>
-                <input id="inp-price" placeholder="Price within block"></input>
-            </div>
-            <button id="btn-add-block" onClick={() => {addBlockHandler()}}>ADD</button>
-            <p id="api-result"></p>
-        </div>
-        <div id="delete-block">
-            <div id="blockID">
-                <h2>BLOCK ID</h2>
-                <input id="inp-blockID" placeholder="Block ID Number"></input>
-            </div>
-            <button id="btn-delete-block" onClick={() => {deleteBlockHandler()}}>DELETE</button>
-        </div>        
-    </div>
     )
 }
 
