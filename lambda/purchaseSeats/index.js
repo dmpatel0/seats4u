7a18ce989dd97b4cf6e6fa6ee3208bbae02b59b7
Akshay Shinde <akshayshinde@autoreg-3609244.dyn.wpi.edu>
2023-12-15 18:18:47 -0500

    modified PurchaseSeats to check Show Date

--- lambda/purchaseSeats/index.js
+++ lambda/purchaseSeats/index.js
@@ -28,6 +28,24 @@ exports.handler = async (event) => {
         })
     })
    }
+   
+   let checkActive = (showID) => {
+        return new Promise((resolve, reject) => {
+            pool.query("SELECT Shows.isActive FROM Shows WHERE showID = ? AND showTime >= CONVERT_TZ(CURRENT_TIMESTAMP, 'UTC', 'US/Eastern')", [showID], (error, rows) => {
+                if (error) { return reject(error); }
+                if ((rows) && (rows.length == 1)) {
+                    if(rows[0].isActive === 1){
+                        return resolve(true); 
+                    }
+                    else{
+                        return resolve(false);
+                    }
+                } else {
+                    return reject("Show not found or Show time past current Date");
+                }
+            });
+        });
+    }
 
    
    //check if any seat is purchased
@@ -52,7 +70,7 @@ exports.handler = async (event) => {
 
    let updateSeats = (status, showID, rowNum, colNum) => {
         return new Promise((resolve, reject) => {
-            pool.query("update seats set isPurchased = ? where showID = ? AND rowNum = ? AND colNum = ?", [status, showID, rowNum, colNum], (error, rows) => {
+            pool.query("update Seats set isPurchased = ? where showID = ? AND rowNum = ? AND colNum = ?", [status, showID, rowNum, colNum], (error, rows) => {
                 if(error){
                     return reject(error);
                 }
@@ -67,69 +85,74 @@ exports.handler = async (event) => {
    }
 
    let response = undefined;
-
-   for(let k = 0; k < event.seats.length;k ++){
-        try{
-            let valid = await validateSeat(event.showID, event.seats[k].rowNum, event.seats[k].colNum);
-            if(valid){
-            let seatStatus = await isSeatPurchased(event.showID, event.seats[k].rowNum, event.seats[k].colNum);
-            if(!seatStatus){
-                response ={
-                    statusCode: 400,
-                    error: "Seat with row  " + event.seats[k].rowNum + " and column " + event.seats[k].colNum + " has been purchased. Refresh the page and try again."
+   try{
+        let validShow = await checkActive(event.showID);
+        if(validShow){
+            for(let k = 0; k < event.seats.length;k ++){
+                try{
+                    let valid = await validateSeat(event.showID, event.seats[k].rowNum, event.seats[k].colNum);
+                    if(valid){
+                    let seatStatus = await isSeatPurchased(event.showID, event.seats[k].rowNum, event.seats[k].colNum);
+                    if(!seatStatus){
+                        response ={
+                            statusCode: 401,
+                            error: "Seat with row  " + event.seats[k].rowNum + " and column " + event.seats[k].colNum + " has been purchased. Refresh the page and try again."
+                        }
+                        break;
+                    }
+                    }
+                    else{
+                        response = {
+                            statusCode: 400,
+                            error: "Seat does not exist"
+                        }
+                        break;
+                    }
                 }
-                break;
-            }
-        }
-        else{
-            response = {
-                statusCode: 400,
-                error: "Seat does not exist"
-            }
-            break;
-        }
-        }
-        catch(error){
-             response ={
-                statusCode: 400,
-                error: "error"
-            }
-            break;
-        }
-   }
-   if(!response){
-   for(let k = 0; k < event.seats.length; k++){
-        try{
-            
-            let updateSeat = await updateSeats(1, event.showID, event.seats[k].rowNum, event.seats[k].colNum);
-            if(!updateSeat){
-                response = {
-                    statusCode: 400,
-                    error: "Update not successful"
+                catch(error){
+                     response ={
+                        statusCode: 400,
+                        error: "error"
+                    }
+                    break;
                 }
-                break;
+           }
+           if(!response){
+                for(let k = 0; k < event.seats.length; k++){
+                        try{
+                            
+                            let updateSeat = await updateSeats(1, event.showID, event.seats[k].rowNum, event.seats[k].colNum);
+                            if(!updateSeat){
+                                response = {
+                                    statusCode: 400,
+                                    error: "Update not successful"
+                                }
+                                break;
+                            }
+                        }
+                        catch(error){
+                            response = {
+                                statusCode: 400,
+                                error: error
+                            }
+                            break;
+                        }
+                    
+                }
+                if(!response){
+                    response = {
+                            statusCode: 200,
+                            success: "All seats have been purchased."
+                    }
+                }
             }
         }
-        catch(error){
-            response = {
-                statusCode: 400,
-                error: error
-            }
-            break;
-        }
-       
-   }
-   if(!response){
-   response = {
-        statusCode: 200,
-        success: "All seats have been purchased."
-   }
-}
-}
-   
-  
+   }catch(err){
+    response = {
+        statusCode: 400,
+        error: JSON.stringify(err)
+    }
+   }  
    pool.end();
    return response;
-
-
 }
